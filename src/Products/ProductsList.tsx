457eb167c49a600ac7f0dc86ddbfa682dbc2a098
ddewe <ddewee92@gmail.com>
2021-10-18 09:33:33 +0200

    added RemoveProduct

--- src/Products/ProductsList.tsx
+++ src/Products/ProductsList.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getProductsSelector } from "./products.slice";
+import { useAppDispatch } from "../store.hooks";
+import { getProductsSelector, removeProduct } from "./products.slice";
 
-interface ProductsListProps {}
-
-const ProductsList: React.FC<ProductsListProps> = ({}) => {
+const ProductsList: React.FC = () => {
     const products = useSelector(getProductsSelector);
+    const dispatch = useAppDispatch();
+
+    const removeFromStore = (id: string) => {
+        dispatch(removeProduct(id));
+    };
 
     return (
         <div>
@@ -13,6 +17,9 @@ const ProductsList: React.FC<ProductsListProps> = ({}) => {
             {products.map((product) => (
                 <div key={product.id}>
                     <span>{`${product.title} : ${product.price}`}</span>
+                    <button onClick={() => removeFromStore(product.id)}>
+                        Remove game
+                    </button>
                 </div>
             ))}
         </div>
--- src/Products/products.slice.ts
+++ src/Products/products.slice.ts
@@ -30,12 +30,15 @@ const productSlice = createSlice({
     initialState,
     reducers: {
         addProduct: (state, action: PayloadAction<Product>) => {
-            return [action.payload, ...state];
+            state.push(action.payload);
+        },
+        removeProduct: (state, action: PayloadAction<string>) => {
+            return state.filter((product) => product.id !== action.payload);
         },
     },
 });
 
-export const { addProduct } = productSlice.actions;
+export const { addProduct, removeProduct } = productSlice.actions;
 
 export const getProductsSelector = (state: RootState) => state.products;
 
